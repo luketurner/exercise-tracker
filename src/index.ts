7216257713144b469f04eb0cb0aae9f241ad788c
Luke Turner <github@luketurner.org>
2024-12-07 12:35:00 -0800

    express initial configuration with pug

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
