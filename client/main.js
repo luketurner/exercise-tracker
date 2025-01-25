0c273f3a806a1d5d4537e66e0e1368212b390486
Luke Turner <github@luketurner.org>
2025-01-25 13:41:36 -0800

    render charts for each parameter

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 import { createAuthClient } from "better-auth/client";
+import Chart from "chart.js/auto";
+
 // import "@starfederation/datastar";
 export const authClient = createAuthClient({
   baseURL: "http://localhost:3000", // the base url of your auth server
@@ -21,4 +23,34 @@ export async function signOut() {
   }
 }
 
-window.exerciseTracker = { signIn, signOut };
+export async function buildChart(exercise, sets) {
+  for (const param of exercise.parameters) {
+    new Chart(document.getElementById(`chart-${param.id}`), {
+      type: "line",
+      data: {
+        labels: sets.map((set) => set.date),
+        datasets: [
+          {
+            label: param.name,
+            data: sets.map((set) => set.parameters[param.id]),
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y:
+            param.dataType === "intensity"
+              ? {
+                  type: "category",
+                  labels: ["high", "medium", "low"],
+                }
+              : {
+                  type: "linear",
+                },
+        },
+      },
+    });
+  }
+}
+
+window.exerciseTracker = { signIn, signOut, buildChart };
